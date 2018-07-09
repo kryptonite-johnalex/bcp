d2df370c74d805c1fda533cd8f3a2b28c8730475
John Alex <ladrajohnalex@gmail.com>
2018-07-09 13:54:12 +0800

    Update landing page

--- index.php
+++ index.php
@@ -25,32 +25,25 @@
 <div class="w3-main" style="margin-left:340px;margin-right:40px">
 
   <!-- Header -->
-<!--   <div class="w3-container" style="margin-top:80px" id="top">
-    <h1 class="w3-jumbo"><b>Queensland Urban Utilities</b></h1>
-    <h1 class="w3-xxxlarge w3-text-red"><b>Initial Screen</b></h1>
+  <div class="w3-container" style="margin-top:80px" id="top">
+    <h1 class="w3-jumbo"><b>Welcome to Vici BCP</b></h1>
+    <h1 class="w3-xxxlarge w3-text-red"><b>Landing Page</b></h1>
     <hr style="width:50px;border:5px solid red" class="w3-round">
-    <p>Thank you for calling Queensland Urban Utilities, my name is , how may I help you?</p>
-    <p>Reminder - Refer to UKS On All Calls!</p>
-    <p>If you are on hold to seniors for over one minute, hang up and use the email button to send an email instead.</p>
-    <p>If caller is unknown or does not wish to be identified, requestor details should be anon.
-    <p>Orders - BW Status - Remember that you need to check both of the boxes that appear when you set a BW for it to write to the system properly.</p>
-    <p>Validating Addresses - DO NOT create or ignore when checking addresses. Refer advice on handout.</p>
-    <p>Seniors Line - Always have the name and telephone number of the person you are transferring.</p>
-    <p>Requestor Tab - Remember to Always Save at this screen. Refer advice on handout.</p>
+    <p></p>
   </div>
 
   <div class="w3-container" id="options" style="margin-top:75px">
     <button type="button" onclick="" class="w3-button w3-block w3-padding-24 w3-red w3-margin-bottom">No Water, Sudden Drop in Pressure, Burst Main, Dirty Water</button>
-    <button type="button" onclick="window.location.href = '/escalation.php'" class="w3-button w3-block w3-padding-24 w3-orange w3-margin-bottom">All Other A1 and A2 Jobs That Needs to be Logged</button>
+    <button type="button" onclick="window.location.href = '/quu/escalation.php'" class="w3-button w3-block w3-padding-24 w3-orange w3-margin-bottom">All Other A1 and A2 Jobs That Needs to be Logged</button>
     <div class="w3-row">
       <div class="w3-half">
-        <button type="button" onclick="window.location.href = '/escalation.php'" class="w3-button w3-block w3-padding-32 w3-yellow w3-margin-bottom">All Other Calls</button>
+        <button type="button" onclick="window.location.href = '/quu/escalation.php'" class="w3-button w3-block w3-padding-32 w3-yellow w3-margin-bottom">All Other Calls</button>
       </div>
       <div class="w3-half" style="padding-left: 215px;">
-        <button type="button" onclick="window.location.href = '/senior.php'" class="w3-button w3-block w3-padding-32 w3-green w3-margin-bottom">Send Senior Email</button>
+        <button type="button" onclick="window.location.href = '/quu/senior.php'" class="w3-button w3-block w3-padding-32 w3-green w3-margin-bottom">Send Senior Email</button>
       </div>
     </div>
-  </div> -->
+  </div>
 
 
 <!-- End page content -->
