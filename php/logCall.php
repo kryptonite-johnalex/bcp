ded589087ef2cf7236cdafec3c23c12114c17bd3
John Alex <ladrajohnalex@gmail.com>
2018-08-10 00:08:54 +0800

    Update db script for Riskman

--- php/logCall.php
+++ php/logCall.php
@@ -18,34 +18,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$agent = $_SESSION['agent'];
     $phone = $_SESSION['phone'];
 
-
-	if($type == 'log' && $campaign == 'dcsi') {
+	if ($type == 'log' && $campaign == 'dcsi') {
 
 		$details = $_POST['details'];
-        $property_num = $_POST['property_num'];
-        $chintaro = isset($_POST['chintaro']) ? 1 : 0;
-        $chintaro_option = isset($_POST['chintaroList']) ? $_POST['chintaroList'] : "";
-        $reminder = isset($_POST['reminder']) ? $_POST['reminder'] : "";
+		$property_num = $_POST['property_num'];
+		$chintaro = isset($_POST['chintaro']) ? 1 : 0;
+		$chintaro_option = isset($_POST['chintaroList']) ? $_POST['chintaroList'] : "";
+		$reminder = isset($_POST['reminder']) ? $_POST['reminder'] : "";
 
+		// Timestamp when the process is created.
+		$created_at = date("Y-m-d H:i:s");
 
-        // Timestamp when the process is created.
-    	$created_at = date("Y-m-d H:i:s");
-
-		var_dump($_POST);
 		$sql = "INSERT INTO dcsi_log (`agent_name`, `phone`, `details`, `property_num`, `chintaro`, `chintaro_option`, `reminder`, `created_at`, `sent_status`) VALUES ('$agent', '$phone', '$details', '$property_num', '$chintaro', '$chintaro_option', '$reminder', '$created_at', '$sent_status')";
 
-		if ($conn->query($sql) === TRUE) {
-        // echo "New record created successfully";
-	    } else {
-	        echo "Error: " . $sql . "<br>" . $conn->error;
-	    }
+	} elseif ($type == 'log' && $campaign == 'riskman'){
 
-	    $conn->close();
+		$disability_service = $_POST['option'];
 
+		if ($disability_service == 'yes') {
+			$supervisor_notified = $_POST['optionYes'];
+			$working_today = '-';
+		} else {
+			$supervisor_notified = '-';
+			$working_today = $_POST['optionNo'];
+		}
+
+		$caller_type = $_POST['caller_type'];
+		$incident_report = $_POST['incident_report'];
+		$report_num = $_POST['report_num'];
+
+		// Timestamp when the process is created.
+		$created_at = date("Y-m-d H:i:s");
+
+		$sql = "INSERT INTO riskman_log (`agent_name`, `phone`, `disability_service`, `supervisor_notified`, `working_today`, `caller_type`, `incident_report`, `report_num`, `created_at`, `sent_status`) VALUES ('$agent', '$phone', '$disability_service', '$supervisor_notified', '$working_today', '$caller_type', '$incident_report', '$report_num', '$created_at', '$sent_status')";
+
+	} else {
+		echo "<h1>Error! Please contact Development Engineer ASAP</h1>";
 	}
-}  else {
-    // Not a POST request, set a 403 (forbidden) response code.
-    http_response_code(403);
-    echo "There was a problem with your submission, please try again.";
+
+	if ($conn->query($sql) === TRUE) {
+	// echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
+	$conn->close();
+} else {
+	// Not a POST request, set a 403 (forbidden) response code.
+	http_response_code(403);
+	echo "There was a problem with your submission, please try again.";
 }
 ?>
