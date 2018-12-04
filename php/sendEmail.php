de369905b30771f43dc1f1fc7614523a77e8efb6
John Alex <ladrajohnalex@gmail.com>
2018-12-04 14:41:15 +0800

    Update sendEmail.php

--- php/sendEmail.php
+++ php/sendEmail.php
@@ -184,7 +184,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email_headers .= "From: $name <$email>";
 
     // Send the email.
-    if (mail($recipient, $subject, $email_content, $email_headers)) {
+    if (mail($recipient, $subject, $email_content, $email_headers, '-f service@contact121.com.au')) {
         // Set a 200 (okay) response code.
         http_response_code(200);
         echo "Thank You! Your message has been sent.";
